4992cbfa4113e320d0a30a4d03a8138ef376a76c
Tuans09 <dt2003911@gmail.com>
2025-05-23 17:31:52 +0700

    xay dung ui phan sidebar 1

---
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,41 @@
+import classNames from "classnames/bind";
+import styles from "./Sidebar.module.scss";
+import config from "~/config";
+import Menu, { MenuItem } from "./Menu";
+import {
+    HomeIcon,
+    HomeActiveIcon,
+    UserFollowingActiveIcon,
+    LiveActiveIcon,
+    UserFollowingIcon,
+    LiveIcon,
+} from "~/components/Icons";
+const cx = classNames.bind(styles);
+function Sidebar() {
+    return (
+        <aside className={cx("wrapper")}>
+            <Menu>
+                <MenuItem
+                    title="For You"
+                    to={config.routes.home}
+                    icon={<HomeIcon />}
+                    activeIcon={<HomeActiveIcon />}
+                ></MenuItem>
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserFollowingIcon />}
+                    activeIcon={<UserFollowingActiveIcon />}
+                ></MenuItem>
+                <MenuItem
+                    title="Live"
+                    to={config.routes.live}
+                    icon={<LiveIcon />}
+                    activeIcon={<LiveActiveIcon />}
+                ></MenuItem>
+            </Menu>
+        </aside>
+    );
+}
+
+export default Sidebar;
